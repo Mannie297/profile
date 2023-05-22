f2c269ee5f21a59859f2ba44c21fdaec34414360
mannie297 <dikson297@gmail.com>
2023-05-22 01:05:55 +0100

    thirdydy commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -60,14 +60,7 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
+    
     {
       name: "JavaScript",
       icon: javascript,
