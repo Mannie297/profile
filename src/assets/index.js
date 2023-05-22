5d3c8355cf4d12ca4333202e79f6db97e0b7d002
mannie297 <dikson297@gmail.com>
2023-05-22 15:38:46 +0100

    thidnff commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -28,7 +28,7 @@ import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
-import proone from "./proone.png"
+import proone from "./proone.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
